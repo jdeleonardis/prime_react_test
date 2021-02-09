3f77f29d0490ba1e8663c2bf5ac2cda15788efb1
jdeleonardis <jdeleonardis@hotmail.com>
2021-02-09 11:43:14 -0500

    initial version testing prime-react library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import MenubarDemo from './MenubarDemo'
+import testpage from './testpage'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <MenubarDemo />
+//     </div>
+//   );
+// }
 
-export default App;
+ function App() {
+   return (
+      <BrowserRouter>
+        <div>        
+            <Switch>
+                <Route exact path="/" component={MenubarDemo} />
+                <Route exact path="/testpage" component={testpage} />
+            </Switch>
+         </div>
+
+     </BrowserRouter>
+   );
+ }
+ export default App;
+
---
+++ src/testpage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Menubar } from 'primereact/menubar';
+import { InputText } from 'primereact/inputtext';
+import 'primeicons/primeicons.css';
+import 'primereact/resources/primereact.css';
+import './index.css'
+// import 'primeflex/primeflex.css';
+
+const testpage = () => {
+
+
+    return (
+        <div>
+            <div>
+                test test test
+            </div>
+        </div>
+    );
+}
+
+export default testpage
